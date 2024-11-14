7642ce28a34d9450cc8e7fbb1d6eb92864d68e30
Ikrombek Olloberdiyev <Ikpydev@gmail.com>
2024-11-14 22:27:25 +0500

    bug

--- lib/ui/category_menu/category_menu_item.dart
+++ lib/ui/category_menu/category_menu_item.dart
@@ -17,73 +17,73 @@ class CategoryMenuItem extends StatelessWidget {
   Widget build(BuildContext context) {
     // print(data.slug);
     return GestureDetector(
-      onTap: () {
-        var category =
-            CategoryItemData(slug: data.slug ?? "", title: data.name ?? "");
+        onTap: () {
+          var category = CategoryItemData(slug: data.slug ?? "", title: data.name ?? "");
 
-        // Navigator.pushNamed(context, '/category', arguments: category);
-      },
-      child:ExpansionTile(title:  Container(
-        margin: const EdgeInsets.all(12),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          // Navigator.pushNamed(context, '/category', arguments: category);
+        },
+        child: ExpansionTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          title: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                data.icon!.endsWith('.svg')
+                    ? SvgPicture.network(
+                        data.icon ?? '',
+                        height: 28,
+                        width: 28,
+                        // fit: BoxFit.none,
+                      )
+                    : CachedNetworkImage(
+                        imageUrl: data.icon ?? '',
+                        height: 28,
+                        width: 28,
+                        // fit: BoxFit.none,
+                      ),
+                SizedBox(width: 4),
+                Expanded(
+                  child: Text(
+                    data.name ?? '',
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(width: 4),
+              ],
+            ),
+          ),
           children: [
-            data.icon!.endsWith('.svg')
-                ? SvgPicture.network(
-              data.icon ?? '',
-              height: 28,
-              width: 28,
-              // fit: BoxFit.none,
-            )
-                : CachedNetworkImage(
-              imageUrl: data.icon ?? '',
-              height: 28,
-              width: 28,
-              // fit: BoxFit.none,
-            ),
-            SizedBox(width: 4),
-            Expanded(
-              child: Text(
-                data.name ?? '',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleSmall
-                    ?.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(width: 4),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: data.childs?.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  contentPadding: EdgeInsets.only(left: 32, right: 16),
+                  title: Text(data.childs?[index].name ?? ''),
+                  onTap: () {
+                    var category =
+                        CategoryItemData(slug: data.childs?[index].slug ?? "", title: data.childs?[index].name ?? "");
 
-          ],
-        ),
-      ),
-        children: [
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: data.childs?.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(data.childs?[index].name ?? ''),
-                onTap: () {
-                  var category = CategoryItemData(
-                      slug: data.childs?[index].slug ?? "",
-                      title: data.childs?[index].name ?? "");
-
-                  Navigator.push(
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => BlocProvider(
-                            create: (c) => CategoryBloc()..add(GetSlugCategoryEvent(slug: category.slug)),
-                            child: CategoryScreen(
-                              data: category,
-                            ),
-                          )));
-                },
-              );
-            },
-          )
-        ],
-      )
-    );
+                        builder: (context) => BlocProvider(
+                          create: (c) => CategoryBloc()..add(GetSlugCategoryEvent(slug: category.slug)),
+                          child: CategoryScreen(
+                            data: category,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            )
+          ],
+        ));
   }
 }
